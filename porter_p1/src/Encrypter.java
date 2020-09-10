e4a8f051291eff3a6ac46e0afec08f648f7c7db9
cliffporter <cliff@cliffporter.net>
2020-09-10 17:44:07 -0400

    Added default constructor and related funcionality

--- porter_p1/src/Encrypter.java
+++ porter_p1/src/Encrypter.java
@@ -1,16 +1,21 @@
 public class Encrypter
+
 {
     //Instance vars
     private int[] nums;
 
     //Constructor
+    public Encrypter()
+    {
+        nums = new int[4];
+    }
     public Encrypter(String unEncrypted)
     {
         //Convert string to an int array, store in the instance var nums
-        nums=new int[unEncrypted.length()];
-        for(int i=0; i<unEncrypted.length(); i++)
+        nums = new int[unEncrypted.length()];
+        for (int i = 0; i < unEncrypted.length(); i++)
         {
-            nums[i]=Character.getNumericValue(unEncrypted.charAt(i));
+            nums[i] = Character.getNumericValue(unEncrypted.charAt(i));
         }
     }
 
@@ -18,11 +23,20 @@ public class Encrypter
     public String toString()
     {
         //Create String from nums
-        String rtrn="";
-        for(int i:nums)
+        String rtrn = "";
+        for (int i : nums)
             rtrn += i;
 
         return rtrn;
+    }
+
+    public void setString(String unEncrypted)
+    {
+        nums = new int[unEncrypted.length()];
+        for (int i = 0; i < unEncrypted.length(); i++)
+        {
+            nums[i] = Character.getNumericValue(unEncrypted.charAt(i));
+        }
     }
 
     //Encryption functions
@@ -36,26 +50,36 @@ public class Encrypter
 
         return this.toString();
     }
+    public String encrypt(String str)
+    {
+        this.setString(str);
+
+        modNums();
+
+        swapPositions();
+
+        return this.toString();
+    }
 
     private void modNums()
     {
         //Each num +7, mod 10
-        for(int i=0; i<nums.length; i++)
+        for (int i = 0; i < nums.length; i++)
         {
-            nums[i]=((nums[i]+7)%10);
+            nums[i] = ((nums[i] + 7) % 10);
         }
     }
 
     private void swapPositions()
     {
         //Swap 1 (pos 1 with pos 3)
-        int temp=nums[0];
-        nums[0]=nums[2];
-        nums[2]=temp;
+        int temp = nums[0];
+        nums[0] = nums[2];
+        nums[2] = temp;
 
         //Swap 2 (pos 2 with pos 4)
-        temp=nums[1];
-        nums[1]=nums[3];
-        nums[3]=temp;
+        temp = nums[1];
+        nums[1] = nums[3];
+        nums[3] = temp;
     }
 }
