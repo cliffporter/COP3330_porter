53f06c1e8e40ce41a26237c1aa8049554108319a
cliffporter <cliff@cliffporter.net>
2020-11-29 13:09:11 -0500

    ContactItem code and tests (not tested)

--- porter_p5/src/ContactItem.java
+++ porter_p5/src/ContactItem.java
@@ -1,4 +1,49 @@
 public class ContactItem
 {
+	private String firstName;
+	private String lastName;
+	private String emailAddress;
+	private String phoneNumber;
 
+	public ContactItem(String firstName, String lastName, String emailAddress, String phoneNumber)
+	{
+		if(firstName.length()>0||lastName.length()>0||emailAddress.length()>0||phoneNumber.length()>0)
+		{
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.emailAddress = emailAddress;
+			this.phoneNumber = phoneNumber;
+		}
+		else
+		{
+			throw new IllegalArgumentException("A Contact Item must have at least one non-null value");
+		}
+	}
+
+
+	public String getFirstName() { return firstName; }
+	public String getLastName() { return lastName; }
+	public String getEmailAddress() { return emailAddress; }
+	public String getPhoneNumber() { return phoneNumber; }
+
+	public void editContact(String newFirstName, String newLastName, String newEmailAddress, String newPhoneNumber)
+	{
+		if(newFirstName.length()>0||newLastName.length()>0||newEmailAddress.length()>0||newPhoneNumber.length()>0)
+		{
+			this.firstName = newFirstName;
+			this.lastName = newLastName;
+			this.emailAddress = newEmailAddress;
+			this.phoneNumber = newPhoneNumber;
+		}
+		else
+		{
+			throw new IllegalArgumentException("A Contact Item must have at least one non-null value");
+		}
+	}
+
+	@Override
+	public String toString()
+	{
+		return firstName + " " + lastName + "\n email: " + emailAddress + " phone: " + phoneNumber;
+	}
 }
--- porter_p5/src/ContactItemTest.java
+++ porter_p5/src/ContactItemTest.java
@@ -1,70 +1,82 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ContactItemTest
 {
     @Test
     public void creationFailsWithAllBlankValues()
     {
-
+        assertThrows(IllegalArgumentException.class, () -> new ContactItem("","","",""));
     }
     @Test
     public void creationSucceedsWithBlankEmail()
     {
-
+        assertDoesNotThrow(() -> new ContactItem("na", "me", "", "123"));
     }
     @Test
     public void creationSucceedsWithBlankFirstName()
     {
-
+        assertDoesNotThrow(() -> new ContactItem("", "me", "e@ma.il", "123"));
     }
     @Test
     public void creationSucceedsWithBlankLastName()
     {
-
+        assertDoesNotThrow(() -> new ContactItem("na", "", "e@ma.il", "123"));
     }
     @Test
     public void creationSucceedsWithBlankPhone()
     {
-
+        assertDoesNotThrow(() -> new ContactItem("na", "me", "e@ma.il", ""));
     }
     @Test
     public void creationSucceedsWithNonBlankValues()
     {
-
+        assertDoesNotThrow(() -> new ContactItem("na", "me", "e@ma.il", "123"));
     }
     @Test
     public void editingFailsWithAllBlankValues()
     {
-
+        ContactItem con = new ContactItem("na", "me", "e@ma.il", "123");
+        assertThrows(IllegalArgumentException.class, () -> con.editContact("", "", "", ""));
     }
     @Test
     public void editingSucceedsWithBlankEmail()
     {
+        ContactItem con = new ContactItem("a", "", "", "");
+        assertDoesNotThrow(() -> con.editContact("na", "me", "", "123"));
 
     }
     @Test
     public void editingSucceedsWithBlankFirstName()
     {
-
+        ContactItem con = new ContactItem("a", "", "", "");
+        assertDoesNotThrow(() -> con.editContact("", "me", "e@ma.il", "123"));
     }
     @Test
     public void editingSucceedsWithBlankLastName()
     {
-
+        ContactItem con = new ContactItem("a", "", "", "");
+        assertDoesNotThrow(() -> con.editContact("na", "", "e@ma.il", "123"));
     }
     @Test
     public void editingSucceedsWithBlankPhone()
     {
-
+        ContactItem con = new ContactItem("a", "", "", "");
+        assertDoesNotThrow(() -> con.editContact("na", "me", "e@ma.il", ""));
     }
     @Test
     public void editingSucceedsWithNonBlankValues()
     {
-
+        ContactItem con = new ContactItem("a", "", "", "");
+        assertDoesNotThrow(() -> con.editContact("na", "me", "e@ma.il", "123"));
     }
     @Test
     public void testToString()
     {
-
+        ContactItem con = new ContactItem("na", "me", "e@ma.il", "123");
+        assertEquals("na me" + " email: e@ma.il phone: 123",
+                con.toString().replace("\n", "").replace("\r","")
+        );
     }
 }
